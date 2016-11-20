18c051be467317acea73b36275f0a5082ea0ff11
Daniel Huffman <drhuffman12@yahoo.com>
2016-11-19 21:52:23 -0500

    refactor to run from the top folder and add some sorting related comments

--- the_challenge/fyi/test.rb
+++ the_challenge/fyi/test.rb
@@ -1,12 +1,18 @@
 letters = {}
 syms = {}
 
-# file = File.read('the_challenge/input.txt', encoding: 'iso-8859-1')
-file = File.read('../input.txt')
+# NOTE: for 'stable' sort (i.e.: where two values could be considered equal and you don't want to swap the order),
+#   you have to do something like `arr.sort_by.with_index { |o, i| [o, i] }`
+# See:
+# * https://bugs.ruby-lang.org/issues/1089
+# * https://github.com/crystal-lang/crystal/issues/2350"`
+
+file = File.read('the_challenge/input.txt')
 file.each_line do |line|
   begin
     chars = line.gsub(/\r?\n/,'').split('')
     chars.each do |char|
+      # if char.match(/([^\x{0000}-\x{007F}]|\w)/)
       if char.match(/([^\u0000-\u007F]|\w)/)
         letters[char] ||= 0
         letters[char] += 1
@@ -31,5 +37,5 @@ def rpt(title, hist)
   report_lines
 end
 
-report_lines = rpt("letters:", letters) + rpt("symbols:", syms)
-File.open('test.out', 'w') {|f| f.write(report_lines.join("\n")) }
+report_lines = rpt("symbols:", syms) + rpt("letters:", letters)
+File.open('the_challenge/fyi/test.out', 'w') {|f| f.write(report_lines.join("\n")) }
